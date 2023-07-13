8df68e077381483a962749f718992cf357621eda
Djoni Bourscheid <djonibourscheid00@gmail.com>
2023-07-13 13:40:02 -0300

    usando map

---
+++ 03-Arrays/14-usando-map-em-strings.js
@@ -0,0 +1,6 @@
+// Padronize para ter maior consistência nos nossos dados.
+const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+
+const nomesAtualizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesAtualizados);
