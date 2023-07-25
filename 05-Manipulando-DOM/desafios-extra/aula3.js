19c47b2be7d9a4adfda975f0ca5f23b6ba7ef656
Djoni Bourscheid <djonibourscheid00@gmail.com>
2023-07-25 19:14:59 +0000

    desafio aula 3

---
+++ 05-Manipulando-DOM/desafios-extra/aula3.js
@@ -0,0 +1,8 @@
+// Remova a cor “Vermelho”;
+// Utilize console.log(lista); ao final para conferir o resultado.
+
+var lista = ["Laranja", "Vermelho", "Branco", "Amarelo", "Rosa"];
+
+lista.splice(1, 1);
+
+console.log(lista);
