b899d0f71c2ff86673ec7d69c1ee86f52c69c8c8
Djoni Bourscheid <djonibourscheid00@gmail.com>
2023-07-30 04:23:31 +0000

    Aulas de repetições

---
+++ 08-Java-JRE-JDK/aula7/TesteLacosBreak.java
@@ -0,0 +1,15 @@
+public class TesteLacosBreak {
+  public static void main(String[] args) {
+    for (int i = 0; i < 11; i++) {
+      for (int j = 0; j < 11; j++) {
+        if (j == i) {
+          break;
+        }
+
+        System.out.print("*");
+      }
+
+      System.out.println();
+    }
+  }
+}
