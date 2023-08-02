e3253bad5a6cb3b01a9eeff6dfec7d45245db364
Djoni Bourscheid <djonibourscheid00@gmail.com>
2023-08-02 16:55:46 +0000

    Aula 1

---
+++ 10-Java-polimorfismo/1-ByteBank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,11 @@
+public class TesteFuncionario {
+  public static void main(String[] args) {
+    Funcionario djoni = new Funcionario();
+    djoni.setNome("Djoni Bourscheid");
+    djoni.setCpf("000.000.000-00");
+    djoni.setSalario(2500.8);
+
+    System.out.println(djoni.getNome());
+    System.out.println(djoni.getBonificacao());
+  }
+}
